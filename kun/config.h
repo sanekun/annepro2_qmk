a22cee55c43c628e07bf4c53379fbc1e7513cc4f
Lelp <Lelp27@github.com>
2022-02-22 00:12:10 +0900

    SHIFT_TOGGLE_LAYER

--- kun/config.h
+++ kun/config.h
@@ -1,3 +1,4 @@
 // Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
 #define HOLD_ON_OTHER_KEY_PRESS
 #define TAPPING_TERM 130
+#define TAPPING_TOGGLE 2
