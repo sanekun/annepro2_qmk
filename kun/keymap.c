23d91f9a82cf68f9ad97170f1d857c44b5df507f
Lelp27 <sanekun@kaist.ac.kr>
2024-08-27 09:50:19 +0900

    update capslock

--- kun/keymap.c
+++ kun/keymap.c
@@ -20,7 +20,7 @@ enum anne_pro_layers {
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_DEL,
     KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(_FN2_LAYER, KC_SCLN), KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP,
-    LT(_FN1_PRESS_LAYER, KC_CAPS), KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+    MO(_FN1_PRESS_LAYER), KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
 ),
  [_DEFAULT_LAYER] = KEYMAP( /* Base */
     _______, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
@@ -34,7 +34,7 @@ enum anne_pro_layers {
     KC_DEL, KC_HOME, KC_UP, KC_END, C(KC_LEFT), _______, _______, KC_HOME, _______, KC_END, KC_BSPC, _______, KC_INS, _______,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, C(KC_RGHT), _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, _______,
     _______, KC_PGUP, KC_PGDN, KC_BSPC, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______
+    KC_CAPS, _______, _______, _______, _______, _______, _______, _______
 ),
  [_NUM1_LAYER] = KEYMAP( /* Base */
     _______, TO(_BASE_LAYER), TO(_FN1_TOGGLE_LAYER), TO(_NUM1_LAYER), TO(_NUM2_LAYER), TO(_MOVE_LAYER), TO(_DEFAULT_LAYER), _______, _______, _______, _______, _______, _______, _______,
